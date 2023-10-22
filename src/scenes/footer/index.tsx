eb117ef5074e19ef1ef83174ecacb69d271e239d
PVSKR <skr.polu@hotmail.com>
2023-10-22 22:55:37 +0530

    Footer section  contact and link part added and  styled.

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = (props: Props) => {
       className="bg-primary-100 py-16"
       >
           <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
-              <div className="mt-16 basis-1/2 md;mt-0">
+              <div className="mt-16 basis-1/2 md:mt-0">
                   <img alt='logo' src={Logo} />  
                   <p className="my-4">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -17,8 +17,22 @@ const Footer = (props: Props) => {
                   </p>
                   <p> &#169; Evogym All Right Reserved. </p>
               </div> 
-              <div></div>   
-         <div></div>     
+              <div className="mt-16 basis-1/4 md:mt-0">
+                  <h4 className="font-bold">
+                   Links   
+                  </h4>
+                  <p className="my-4"> Fitness goal </p>
+                  <p className="my-4"> Workout for health</p>
+                  <p className="my-4"> Yoga to get coolness</p>
+              </div>   
+              <div className="mt-16 basis-1/4 md:mt-0">
+              <h4 className="font-bold">
+                   Contact Us   
+                  </h4>
+                  <p className="my-4"> fitnnes@gmail.com </p>
+                  <p className="my-4"> +91987651234</p>
+                  
+              </div>     
           </div>
           
    </footer>
