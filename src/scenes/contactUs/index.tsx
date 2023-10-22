43c70933ffe46befc2dbc2bebbd96b88e0f7c4e8
PVSKR <skr.polu@hotmail.com>
2023-10-22 16:43:05 +0530

    forma creation completed.

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -8,7 +8,23 @@ type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 };
 
-const ContactUs = ({setSelectedPage}: Props) => {
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+    const inputStyles = `mt-4 w-full rounded-lg bg-primary-500 px-4 py-3 placeholder-white`
+    
+    const {
+        register,
+        
+        trigger,
+        formState: { errors }
+    } = useForm();
+    const onSubmit = async (e: any) => {
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+        
+    }
   return (
       <section id="contactus"
       className="mx-auto w-5/6 pt-24 pb-32"
@@ -38,6 +54,90 @@ const ContactUs = ({setSelectedPage}: Props) => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nam asperiores est libero explicabo itaque reiciendis molestias, magni officia corporis pariatur quo totam accusantium animi fugiat molestiae, delectus, nihil alias!
                   </p>
               </motion.div>
+
+              {/* Form and Image */}
+              <div
+                  className="mt-10  justify-between gap-8 md:flex"
+              >
+                  <motion.div
+                      className="mt-10 basis-3/5 md:mt-0"
+                      initial="hidden"
+                      whileInView='visible'
+                      viewport={{ once: true, amount: 0.5 }}
+                      transition={{ duration: 0.5 }}
+                      variants={{
+                          hidden: { opacity: 0, y:50 },
+                          visible:{opacity:1,y:0},
+                      }}
+                  >
+                      <form
+                          target="_blank"
+                          onSubmit={onSubmit}
+                          method="POST"
+                          action="https://formsubmit.com/sravanpolu.me@gmail.com"
+                          
+                      >
+                          <input className={inputStyles}
+                              type="text"
+                              placeholder="Name"
+                              {...register("name", {
+                                  required: true,
+                                  maxLength:100,
+                              })}
+                          />
+                          {errors.name && (
+                              <p className="mt-1 text-primary-500">
+                                  {errors.name.type === "required" && "This field is required."}
+                                  {errors.name.type==="maxLenght" && "Max length is 100 char."}
+                                  
+                              </p>
+                          )}
+
+<input className={inputStyles}
+                              type="text"
+                              placeholder="Email"
+                              {...register("email", {
+                                  required: true,
+                                  pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                              })}
+                          />
+                          {errors.email && (
+                              <p className="mt-1 text-primary-500">
+                                  {errors.email.type === "required" && "This field is required."}
+                                  {errors.email.type==="pattern" && "Invalid email address."}
+                                  
+                              </p>
+                          )}
+
+                          <textarea
+                              className={inputStyles}
+                              rows={4}
+                              cols={50}
+                       
+                              placeholder="Message"
+                       
+                              {...register("message", {
+                                  required: true,
+                                 maxLength:2000,
+                              })}
+                          />
+                          {errors.message && (
+                              <p className="mt-1 text-primary-500">
+                                  {errors.message.type === "required" && "This field is required."}
+                                  {errors.message.type==="maxLenght" && "Max length is 100 char."}
+                                  
+                              </p>
+                          )}
+                          <button
+                              type="submit"
+                              className="mt-5 rounded-lg bg-secondary-500 px-20 py-3
+                              trasition duaration-500 hover:text-white"
+                          >
+                            SUBMIT  
+                          </button>
+                      </form>
+                  </motion.div>   
+              </div>
           </motion.div>
       </section>
   )
